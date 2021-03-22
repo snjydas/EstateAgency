6b25cf47bf5c13c805db6c1342af5389330d4af1
Vuppala Rakshith <rakshithvuppala@gmail.com>
2021-03-22 17:26:09 +0530

    Customer added getters and setters

--- Real_Estate_Broker_Application/src/main/java/com/cg/entity/Customer.java
+++ Real_Estate_Broker_Application/src/main/java/com/cg/entity/Customer.java
@@ -6,4 +6,39 @@ public class Customer extends User {
 	private int custId;
 	private String custName;
 	private List<Property> properties;
+	
+	public Customer() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public Customer(String userid, String password, String role, String mobile, String email, String city) {
+		super(userid, password, role, mobile, email, city);
+		// TODO Auto-generated constructor stub
+	}
+	public Customer(int custId, String custName, List<Property> properties) {
+		super();
+		this.custId = custId;
+		this.custName = custName;
+		this.properties = properties;
+	}
+	public int getCustId() {
+		return custId;
+	}
+	public void setCustId(int custId) {
+		this.custId = custId;
+	}
+	public String getCustName() {
+		return custName;
+	}
+	public void setCustName(String custName) {
+		this.custName = custName;
+	}
+	public List<Property> getProperties() {
+		return properties;
+	}
+	public void setProperties(List<Property> properties) {
+		this.properties = properties;
+	}
+	
+	
 }
