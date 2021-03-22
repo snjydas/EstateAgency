0838bdafacf5a7d655372a6d5898848467f86c6b
Vuppala Rakshith <rakshithvuppala@gmail.com>
2021-03-22 12:11:32 +0530

    Update Broker.java

--- Real_Estate_Broker_Application/src/main/java/com/cg/entity/Broker.java
+++ Real_Estate_Broker_Application/src/main/java/com/cg/entity/Broker.java
@@ -6,4 +6,18 @@ public class Broker extends User {
 	private int broId;
 	private String broName;
 	private List<Property> properties;
+	public String getBroName() {
+		return broName;
+	}
+	public void setBroName(String broName) {
+		this.broName = broName;
+	}
+	public List<Property> getProperties() {
+		return properties;
+	}
+	public void setProperties(List<Property> properties) {
+		this.properties = properties;
+	}
+	
+	
 }
