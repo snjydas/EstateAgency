1536d93c9025bc1e34987216aefe69f8d9175909
ArshiyaShahina <80845990+ArshiyaShahina@users.noreply.github.com>
2021-03-22 12:36:59 +0530

    Update Deal.java

--- Real_Estate_Broker_Application/src/main/java/com/cg/entity/Deal.java
+++ Real_Estate_Broker_Application/src/main/java/com/cg/entity/Deal.java
@@ -2,10 +2,67 @@ package com.cg.entity;
 
 import java.time.LocalDate;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+@Entity
 public class Deal {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	private int dealId;
 	private LocalDate dealDate;
 	private double dealCost;
 	private Customer customer;
 	private Property property;
+	
+	public Deal(LocalDate dealDate, double dealCost, Customer customer, Property property) {
+		super();
+	
+		this.dealDate = dealDate;
+		this.dealCost = dealCost;
+		this.customer = customer;
+		this.property = property;
+	}
+
+	public int getDealId() {
+		return dealId;
+	}
+
+	public void setDealId(int dealId) {
+		this.dealId = dealId;
+	}
+
+	public LocalDate getDealDate() {
+		return dealDate;
+	}
+
+	public void setDealDate(LocalDate dealDate) {
+		this.dealDate = dealDate;
+	}
+
+	public double getDealCost() {
+		return dealCost;
+	}
+
+	public void setDealCost(double dealCost) {
+		this.dealCost = dealCost;
+	}
+
+	public Customer getCustomer() {
+		return customer;
+	}
+
+	public void setCustomer(Customer customer) {
+		this.customer = customer;
+	}
+
+	public Property getProperty() {
+		return property;
+	}
+
+	public void setProperty(Property property) {
+		this.property = property;
+	}
+	
 }
