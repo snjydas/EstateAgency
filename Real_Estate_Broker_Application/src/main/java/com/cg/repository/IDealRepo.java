45eff7bb9b678dc3a1114ed6013d97aa5f1e5580
ArshiyaShahina <80845990+ArshiyaShahina@users.noreply.github.com>
2021-03-22 12:20:16 +0530

    Update IDealRepo.java

--- Real_Estate_Broker_Application/src/main/java/com/cg/repository/IDealRepo.java
+++ Real_Estate_Broker_Application/src/main/java/com/cg/repository/IDealRepo.java
@@ -2,11 +2,13 @@ package com.cg.repository;
 
 import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+
 import com.cg.entity.Customer;
 import com.cg.entity.Deal;
 import com.cg.entity.Property;
 
-public interface IDealRepo {
+public interface IDealRepo extends JpaRepository<Deal,Integer> {
 
 	public Deal saveDeal(Property property, Customer customer);
 	
