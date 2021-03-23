5faae6d51f675d0139897b2b2d0d85097e7d9da2
Sanjay Das <sanjaydas251297@gmail.com>
2021-03-23 10:32:19 +0530

    Add BrokerNotFoundException

--- Real_Estate_Broker_Application/src/main/java/com/cg/exception/BrokerNotFoundException.java
+++ Real_Estate_Broker_Application/src/main/java/com/cg/exception/BrokerNotFoundException.java
@@ -2,4 +2,11 @@ package com.cg.exception;
 
 public class BrokerNotFoundException extends Exception {
 
+	public BrokerNotFoundException() {
+
+	}
+
+	public BrokerNotFoundException(String message) {
+		super(message);
+	}
 }
