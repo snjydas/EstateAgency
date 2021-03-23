4eee34ee9df6c54c1f764e6dfc2c5c8ab1f4435e
Sanjay Das <sanjaydas251297@gmail.com>
2021-03-23 10:32:04 +0530

    Add CustomerNotFoundException

--- Real_Estate_Broker_Application/src/main/java/com/cg/exception/CustomerNotFoundException.java
+++ Real_Estate_Broker_Application/src/main/java/com/cg/exception/CustomerNotFoundException.java
@@ -1,5 +1,12 @@
 package com.cg.exception;
 
 public class CustomerNotFoundException extends Exception {
+	
+	public CustomerNotFoundException() {
+		
+	}
 
+	public CustomerNotFoundException(String message) {
+		super(message);
+	}
 }
