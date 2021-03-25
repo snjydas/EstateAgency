0507e0c667df78091d9acc520cb1258c5417aa93
Vuppala Rakshith <rakshithvuppala@gmail.com>
2021-03-25 22:07:46 +0530

    Property criteria completed

--- Real_Estate_Broker_Application/src/main/java/com/cg/dao/PropertyDaoImpl.java
+++ Real_Estate_Broker_Application/src/main/java/com/cg/dao/PropertyDaoImpl.java
@@ -4,11 +4,13 @@ import java.util.List;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
 import javax.persistence.TypedQuery;
 
 import com.cg.entity.Property;
 import com.cg.pojo.PropertyCriteria;
 
+import org.hibernate.query.NativeQuery;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -27,12 +29,27 @@ public class PropertyDaoImpl implements PropertyDao {
 
 	@Override
 	public List<Property> fetchPropertyByCriteria(PropertyCriteria criteria) {
-		TypedQuery<Property> q = em.createQuery("select p from Property p where p.configuration =:i and p.offerType=:j and p.city=:k ", Property.class);
-		q.setParameter("i", criteria.getConfig());
-		q.setParameter("j", criteria.getOffer());
-		q.setParameter("k", criteria.getCity());
-		//q.setParameter("l", criteria.getMinCost());
-		//q.setParameter("m", criteria.getMaxCost());
+		String i =criteria.getConfig();
+		String j =criteria.getOffer();
+		String k =criteria.getCity();
+		double l =criteria.getMinCost();
+		double m=criteria.getMaxCost();
+		if(i ==null)
+			i="%";
+		if(j==null)
+			j="%";
+		if(k==null)
+			k="%";
+		if(m==0)
+			m=Double.MAX_VALUE;
+			
+		TypedQuery<Property> q = em.createQuery("select p from Property p where  p.configuration like :i  and p.offerType like:j and  p.city like :k and (p.offerCost Between :l and :m )", Property.class);
+		
+		q.setParameter("i", i);
+		q.setParameter("j", j);
+		q.setParameter("k", k);
+		q.setParameter("l", l);
+		q.setParameter("m", m);
 		return q.getResultList();
 	}
 }
