505cf51069738bdb28b5ea5fd2853835f55cf936
unknown <tripuramalluchaitanya@gmail.com>
2021-03-25 12:33:18 +0530

    sacds

--- Real_Estate_Broker_Application/src/main/java/com/cg/controller/BrokerController.java
+++ Real_Estate_Broker_Application/src/main/java/com/cg/controller/BrokerController.java
@@ -1,5 +1,8 @@
 package com.cg.controller;
 
+
+
+
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,33 +23,42 @@ import com.cg.service.IBrokerService;
 public class BrokerController {
 	@Autowired
 	IBrokerService bService;
-
+	
 	@PostMapping("/add")
 	public Broker addBroker(@RequestBody Broker bro) {
-
+		
 		return bService.addBroker(bro);
 	}
-
 	@PutMapping("/update")
-	public Broker editBroker(@RequestBody Broker bro) throws BrokerNotFoundException {
+	public Broker editBroker(@RequestBody Broker bro) throws BrokerNotFoundException{
+		try {
+			viewBroker(bro.getUserid());
+		}
+		catch(Exception e) {
+			throw new BrokerNotFoundException("Given Broker is inappropriate!");
+		}
 		return bService.editBroker(bro);
 	}
-
 	@DeleteMapping("/remove/{broId}")
-	public Broker removeBroker(@PathVariable int broId) throws BrokerNotFoundException {
+	public Broker removeBroker(@PathVariable int broId) throws BrokerNotFoundException{
+		try {
+			viewBroker(broId);
+		}
+		catch(Exception e) {
+			throw new BrokerNotFoundException("Broker with given Broker ID not found, Please recheck input!");
+		}
 		return bService.removeBroker(broId);
 	}
-
 	@GetMapping("/all")
-	public List<Broker> listAllBrokers() {
+	public List<Broker> listAllBrokers(){
 		return bService.listAllBrokers();
 	}
-
-	@GetMapping("/id/{broId}")
-	public Broker viewBroker(@PathVariable int broId) throws BrokerNotFoundException {
+	@GetMapping("/get/{broId}")
+	public Broker viewBroker(@PathVariable int broId) throws BrokerNotFoundException{
 		try {
 			bService.viewBroker(broId);
-		} catch (Exception e) {
+		}
+		catch(Exception e) {
 			throw new BrokerNotFoundException("Broker with given ID is not found, Please Recheck the input!");
 		}
 		return bService.viewBroker(broId);
--- Real_Estate_Broker_Application/src/main/java/com/cg/service/IBrokerServiceImpl.java
+++ Real_Estate_Broker_Application/src/main/java/com/cg/service/IBrokerServiceImpl.java
@@ -1,21 +1,18 @@
 package com.cg.service;
 
 import java.util.List;
-import java.util.Optional;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 import com.cg.entity.Broker;
 import com.cg.exception.BrokerNotFoundException;
 import com.cg.repository.IBrokerRepo;
 
+
 @Service
-public class IBrokerServiceImpl implements IBrokerService {
+public class IBrokerServiceImpl implements IBrokerService{
 
 	@Autowired
 	IBrokerRepo bDao;
-
 	@Override
 	public Broker addBroker(Broker bro) {
 		bDao.saveAndFlush(bro);
@@ -24,23 +21,15 @@ public class IBrokerServiceImpl implements IBrokerService {
 
 	@Override
 	public Broker editBroker(Broker bro) throws BrokerNotFoundException {
-		try {
-			bDao.findById(bro.getUserid());
-			bDao.saveAndFlush(bro);
-		} catch (Exception e) {
-			throw new BrokerNotFoundException("Given Broker is inappropriate!");
-		}
+		bDao.saveAndFlush(bro);
 		return bro;
 	}
 
 	@Override
 	public Broker removeBroker(int broId) throws BrokerNotFoundException {
-		Optional<Broker> op = bDao.findById(broId);
-		if (op.isPresent()) {
-			bDao.deleteById(broId);
-			return op.get();
-		} else
-			throw new BrokerNotFoundException("Broker with given ID not found,Please Recheck the input!");
+		Broker b = bDao.findById(broId).get();
+		bDao.deleteById(broId);
+		return b;
 	}
 
 	@Override
@@ -52,5 +41,6 @@ public class IBrokerServiceImpl implements IBrokerService {
 	public List<Broker> listAllBrokers() {
 		return bDao.findAll();
 	}
+	
 
 }
