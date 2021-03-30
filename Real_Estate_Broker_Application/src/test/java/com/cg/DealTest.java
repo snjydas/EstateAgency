eaaef6c96900cb2619297362f464ec95b0fdcd95
Sanjay Das <sanjaydas251297@gmail.com>
2021-03-30 10:31:14 +0530

    remove unneccessary imports

--- Real_Estate_Broker_Application/src/test/java/com/cg/DealTest.java
+++ Real_Estate_Broker_Application/src/test/java/com/cg/DealTest.java
@@ -1,18 +1,12 @@
 package com.cg;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-
-import java.time.LocalDate;
-
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-
-import com.cg.entity.Broker;
 import com.cg.entity.Customer;
 import com.cg.entity.Deal;
 import com.cg.entity.Property;
