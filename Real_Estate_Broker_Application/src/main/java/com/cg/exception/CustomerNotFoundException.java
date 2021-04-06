d2baef24dad4b0c654086728339929f988d9d52d
Sanjay Das <sanjaydas251297@gmail.com>
2021-04-06 20:48:25 +0530

    docs: change comments to include credits

--- Real_Estate_Broker_Application/src/main/java/com/cg/exception/CustomerNotFoundException.java
+++ Real_Estate_Broker_Application/src/main/java/com/cg/exception/CustomerNotFoundException.java
@@ -2,7 +2,7 @@ package com.cg.exception;
 
 /*************************************************************************************
  * @author                 SANJAY DAS
- * Description             It is a exception class that handles the exception occuring at 
+ * Description             It is a exception class that handles the exception occurring at 
  *                         service level when a customer is not found.
  * @version                1.0
  * @since           	   24-03-2021
