deb07c4202f96c38188e87ec89883470065e9464
Sanjay Das <sanjaydas251297@gmail.com>
2021-04-21 13:55:00 +0530

    rename: move Rest API to backend folder

---
+++ frontend/src/app/all-properties/all-properties.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-all-properties',
+  templateUrl: './all-properties.component.html',
+  styleUrls: ['./all-properties.component.css']
+})
+export class AllPropertiesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ frontend/src/app/broker-properties/broker-properties.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-broker-properties',
+  templateUrl: './broker-properties.component.html',
+  styleUrls: ['./broker-properties.component.css']
+})
+export class BrokerPropertiesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
