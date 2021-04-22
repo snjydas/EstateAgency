24aa6107888fa55810d8abd7f91c823eaa0f7a4b
Vuppala Rakshith <rakshithvuppala@gmail.com>
2021-04-22 13:19:50 +0530

    Added propertySearch

--- frontend/src/app/customer/property-search/property-search.component.ts
+++ frontend/src/app/customer/property-search/property-search.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { PropertyServiceService } from 'src/app/property-service.service';
 
 @Component({
   selector: 'app-property-search',
@@ -6,10 +9,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./property-search.component.css']
 })
 export class PropertySearchComponent implements OnInit {
+  propForm: FormGroup
 
-  constructor() { }
+  constructor( private fb: FormBuilder, private pService:PropertyServiceService,
+    private router:Router) { }
 
   ngOnInit(): void {
+    this.propForm=this.fb.group({
+      config: ['', Validators.required],
+      offer:['', Validators.required],
+      city:['', Validators.required],
+      minCost:['', Validators.required],
+      maxCost:['', Validators.required]
+    })
   }
+
+  searchProp() {
+    this.pService.getPropertyByCriteria(this.propForm.value).subscribe(res=>{
+      this.router.navigate([''])
+    }, err=>{
+      console.log(err);
+    }
+    );
+    
+  }
+
 
 }
