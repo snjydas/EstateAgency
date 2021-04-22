f765b18b9ee298c8e63a7df2af4bd5dbe9f36ef7
Saradhi9644 <siddareddypartha@gmail.com>
2021-04-22 16:15:20 +0530

    add login.html

--- frontend/src/app/login/login.component.ts
+++ frontend/src/app/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { UserServiceService } from '../user-service.service';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +9,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-
-  constructor() { }
+  message: string;
+  loginForm: FormGroup;
+  constructor(private fb: FormBuilder, private router: Router,
+    private uService: UserServiceService) { }
 
   ngOnInit(): void {
+    this.loginForm = this.fb.group({
+      uname: ['', Validators.required],
+      password: ['', Validators.required]
+    })
   }
+  login() {
+    if (this.uService.login(this.loginForm.value)) {
+      
+      this.router.navigate(['user']);
+    }
+    else {
+      this.message = "Invalid Login, Please check ID and Password";
+      this.router.navigate(['/']);
+    }
 
+  }
 }
