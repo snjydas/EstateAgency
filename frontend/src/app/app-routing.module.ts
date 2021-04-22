6742430533daac253947daa061f966bccbb68ca6
Tripuramallu-Chaitanya <tripuramalluchaitanya@gmail.com>
2021-04-22 12:30:21 +0530

    app routing broker routes

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BrokerComponent } from './broker/broker.component';
+import { UpdateComponent } from './broker/update/update.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path:'',redirectTo:'broker', pathMatch:'full' },
+  {
+    path:'broker', component:BrokerComponent,
+    children:[
+      { path:'update', component:UpdateComponent}
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
