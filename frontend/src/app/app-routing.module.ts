d1cffd51946578d95606f3cbf422e24f8f99994c
Tripuramallu-Chaitanya <tripuramalluchaitanya@gmail.com>
2021-04-22 13:30:56 +0530

    approutes

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { BrokerComponent } from './broker/broker.component';
 import { UpdateComponent } from './broker/update/update.component';
+import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
-  { path:'',redirectTo:'broker', pathMatch:'full' },
+  { path:'',redirectTo:'login', pathMatch:'full' },
+  { path: 'login', component:LoginComponent },
   {
     path:'broker', component:BrokerComponent,
     children:[
