15ab54b19d0d0348cbb4baeb572ded68d3c45a92
Vuppala Rakshith <rakshithvuppala@gmail.com>
2021-04-22 17:36:14 +0530

    Revert "add login.html"
    This reverts commit f765b18b9ee298c8e63a7df2af4bd5dbe9f36ef7.

--- frontend/src/app/login/login.component.ts
+++ frontend/src/app/login/login.component.ts
@@ -1,7 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { Router } from '@angular/router';
-import { UserServiceService } from '../user-service.service';
 
 @Component({
   selector: 'app-login',
@@ -9,26 +6,10 @@ import { UserServiceService } from '../user-service.service';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-  message: string;
-  loginForm: FormGroup;
-  constructor(private fb: FormBuilder, private router: Router,
-    private uService: UserServiceService) { }
+
+  constructor() { }
 
   ngOnInit(): void {
-    this.loginForm = this.fb.group({
-      uname: ['', Validators.required],
-      password: ['', Validators.required]
-    })
   }
-  login() {
-    if (this.uService.login(this.loginForm.value)) {
-      
-      this.router.navigate(['user']);
-    }
-    else {
-      this.message = "Invalid Login, Please check ID and Password";
-      this.router.navigate(['/']);
-    }
 
-  }
 }
