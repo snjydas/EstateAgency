12a060929b2ba5cf57d4c7ef85c6bfa172b50530
Vuppala Rakshith <rakshithvuppala@gmail.com>
2021-04-23 16:49:18 +0530

    Did the login module

--- frontend/src/app/login/login.component.ts
+++ frontend/src/app/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { UserServiceService } from '../user-service.service';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +9,47 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  private id:number;
+  private role:string
+  loginForm:FormGroup
+  private allow:boolean;
 
-  constructor() { }
+  constructor(private fb:FormBuilder, private uService:UserServiceService, private router:Router) { }
 
   ngOnInit(): void {
+    this.loginForm=this.fb.group({
+      userId:['',Validators.required],
+      password:['',Validators.required]
+    })
   }
+
+  getId():number{
+    return this.id
+  }
+
+  login(){
+    this.uService.login(this.loginForm.value).subscribe(data=>{
+      this.allow=data;
+      if(this.allow){
+        this.id=this.loginForm.value.userId
+        console.log(this.id)
+        this.uService.getUserById(this.id).subscribe(data=>{
+          this.role=data.role
+          if(this.role=="Broker"){
+            this.router.navigate(['/broker'])
+          }
+          else{
+            this.router.navigate(['/customer'])
+          }
+        })
+      }
+      else{
+        //alert("Enter correct userId and Password");
+        this.router.navigate(['/login'])
+      }
+      })
+    
+  }
+  
 
 }
