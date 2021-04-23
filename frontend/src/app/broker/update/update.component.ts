6ea0216e0431bd4e0ed6569087fd5aa53e8a82c3
Sanjay Das <sanjaydas251297@gmail.com>
2021-04-23 10:42:07 +0530

    🐛 fix( update): bring back removed code

--- frontend/src/app/broker/update/update.component.ts
+++ frontend/src/app/broker/update/update.component.ts
@@ -1,16 +1,30 @@
 import { Component, OnInit } from '@angular/core';
-
+import { ActivatedRoute, Router } from '@angular/router';
+import { Broker } from '../broker';
+import { BrokerServiceService } from '../broker-service.service';
+import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-update',
-  templateUrl: './update.component.html',
-  styleUrls: ['./update.component.css']
+
 })
 export class UpdateComponent implements OnInit {
   brokid:number;
+  brok:Broker;
 
   constructor() { }
+  constructor(private actRouter:ActivatedRoute, private bService:BrokerServiceService, private router:Router) { }
 
   ngOnInit(): void {
+    this.brok=new Broker();
+    this.brokid=this.actRouter.snapshot.params['userId'];
+    this.bService.getBrokerById(this.brokid).subscribe(data=>{
+      this.brok=data;
+    })
+  }
+  updateData(){
+    this.bService.updateBroker(this.brok).subscribe(res=>{
+      this.router.navigate(['broker']);
+    })
   }
 
 }
