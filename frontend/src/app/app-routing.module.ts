52a290212e02c5d62103c21f9d13b71907f692e6
Tripuramallu-Chaitanya <tripuramalluchaitanya@gmail.com>
2021-04-24 12:09:47 +0530

    routes

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,6 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AllBrokersComponent } from './broker/all-brokers/all-brokers.component';
 import { BrokerComponent } from './broker/broker.component';
+import { CreateComponent } from './broker/create/create.component';
+import { MyPropertiesComponentB } from './broker/my-properties/my-properties.component';
+import { UpdateMypropsComponent } from './broker/update-myprops/update-myprops.component';
+import { UpdateUserComponent } from './broker/update-user/update-user.component';
 import { UpdateComponent } from './broker/update/update.component';
 import { AllPropertyByCriteriaComponent } from './customer/all-property-by-criteria/all-property-by-criteria.component';
 import { CustomerComponent } from './customer/customer.component';
@@ -18,9 +23,15 @@ const routes: Routes = [
   { path: 'login', component:LoginComponent },
   {
     path:'broker', component:BrokerComponent,
-    children:[
-      { path:'update', component:UpdateComponent}
+    children: [
+      { path: 'update/:userid', component:UpdateComponent},
+      { path: 'all', component:AllBrokersComponent},
+      { path: 'add', component:CreateComponent},
+      { path:'myprops', component:MyPropertiesComponentB},
+      {path:'updating/:propid',component:UpdateMypropsComponent},
+      { path: 'updateuser', component:UpdateUserComponent}
     ]
+
   },
   {
     path:'customer',component:CustomerComponent,
